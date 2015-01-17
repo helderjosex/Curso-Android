7f55185b0cd8ac5d8091e884b414d55029705c70
helderjosex <helderjosex@gmail.com>
2015-01-17 15:51:27 -0300

    Aula 17-01-2015

--- HelloWorld/app/src/main/java/br/com/conhecimentodigital/helloworld/HelloWorld.java
+++ HelloWorld/app/src/main/java/br/com/conhecimentodigital/helloworld/HelloWorld.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 /**
- * Created by fernandooliveira on 10/01/15.
+ * Created by helderjose on 10/01/15.
  */
 public class HelloWorld extends Activity {
 
@@ -26,19 +26,31 @@ public class HelloWorld extends Activity {
     }
 
     public void trocarTexto(View view) {
+
         String novoTexto = input.getText().toString();
         texto.setText(novoTexto);
 
         try {
             Thread.sleep(2000);
 
+            // FORMAS DE ABRIR ACTIVITY
+
+            // Intent Explicita - Melhor Performace
             Intent intent = new Intent(this, MainActivity.class);
+
+            //intent.putExtra("nome", novoTexto);
+
+            //  Intent Implicita ( DECLARADO NO ANDROID MANIFEST )
+            //Intent intent = new Intent("ABRIR_MAIN_ACTIVITY");
+            //Intent intent = new Intent();
+            //intent.setAction("ABRIR_MAIN_ACTIVITY");
+
             intent.putExtra("nome", novoTexto);
 
             startActivity(intent);
 
 
-        } catch (InterruptedException e) {
+       } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
