e5e057ef23d35b3a5f541ee7779cc133f48f35c4
Hélder José <helderjosex@gmail.com>
2015-01-24 17:11:54 -0300

    Tela Login Concluida

--- Academico/app/src/main/java/br/com/conhecimentodigital/academico/LoginActivity.java
+++ Academico/app/src/main/java/br/com/conhecimentodigital/academico/LoginActivity.java
@@ -1,17 +1,39 @@
 package br.com.conhecimentodigital.academico;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    private EditText edtLogin;
+    private EditText edtPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtLogin = (EditText) findViewById(R.id.inputLogin);
+        edtPassword = (EditText) findViewById(R.id.inputPassword);
+    }
+
+    public void login (View v){
+        if(edtLogin.getText().toString().equals("admin@cd.com.br") && edtPassword.getText().toString().equals("123")){
+            Toast.makeText(this,"Login efetuado com sucesso",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }else{
+            //Toast.makeText(this,"Não foi possivel efetuar o login",Toast.LENGTH_LONG).show();
+            edtPassword.setError("Senha ou email incorreto(s)");
+        }
     }
 
 
