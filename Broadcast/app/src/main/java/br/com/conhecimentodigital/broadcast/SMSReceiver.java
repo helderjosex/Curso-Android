b43112deab485fe16a4cc16a9979f2a55836b027
Hélder José <helderjosex@gmail.com>
2015-01-24 19:05:51 -0300

    Implementado Metodo onReceive

--- Broadcast/app/src/main/java/br/com/conhecimentodigital/broadcast/SMSReceiver.java
+++ Broadcast/app/src/main/java/br/com/conhecimentodigital/broadcast/SMSReceiver.java
@@ -1,7 +1,39 @@
 package br.com.conhecimentodigital.broadcast;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
 /**
  * Created by cd-108 on 24/01/2015.
  */
-public class SMSReceiver extends Intent {
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        String address = "";
+
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                str += "SMS from " + msgs[i].getOriginatingAddress();
+                str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "\n";
+
+
+                address = msgs[i].getOriginatingAddress();
+                Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
 }
